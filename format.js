e8c0007d6f70bcd61f50ef8feab368a481d1836a
clarkk <clark@electrobeat.dk>
2021-12-16 00:43:43 +0100

    added zerofill

--- format.js
+++ format.js
@@ -2,6 +2,9 @@
 
 (function(root){
 	root.Format = {
-		
+		zerofill(num, width, append){
+			let n = Math.abs(num), zeros = Math.max(0, width - Math.floor(n).toString().length), fill = Math.pow(10, zeros).toString().substr(1);
+			return append ? parseInt(n+fill) : fill+n;
+		}
 	};
 })(this);
