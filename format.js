c8169d5f15856c3f41a9022bea61f2b76a8335e3
clarkk <clark@electrobeat.dk>
2021-12-28 14:49:01 +0100

    fix zerofill issue

--- format.js
+++ format.js
@@ -117,8 +117,8 @@
 		},
 		
 		zerofill(num, width, append){
-			let n = Math.abs(num), zeros = Math.max(0, width - Math.floor(n).toString().length), fill = Math.pow(10, zeros).toString().substr(1);
-			return append ? parseInt(n+fill) : fill+n;
+			let zeros = Math.max(0, width - num.toString().length), fill = Math.pow(10, zeros).toString().substr(1);
+			return append ? num+fill : fill+num;
 		}
 	};
 	
